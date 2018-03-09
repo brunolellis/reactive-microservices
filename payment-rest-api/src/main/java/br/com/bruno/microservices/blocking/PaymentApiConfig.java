d6772191c68282909c4204ba6bcd4653e71352f3
Bruno Lellis <brunolellis@gmail.com>
2018-03-09 00:20:21 -0300

    blocking

--- payment-rest-api/src/main/java/br/com/bruno/microservices/blocking/PaymentApiConfig.java
+++ payment-rest-api/src/main/java/br/com/bruno/microservices/blocking/PaymentApiConfig.java
@@ -1,5 +1,6 @@
 package br.com.bruno.microservices.blocking;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +9,13 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class PaymentApiConfig {
 
+	@Value("${payment.api}")
+	private String paymentApi;
+	
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplateBuilder()
+				.rootUri(paymentApi)
 				.build();
 	}
 }
